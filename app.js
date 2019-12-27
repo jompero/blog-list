a39f8ffcad671ab9473b03c65b5c062d8d367d13
jompero <dani.jompero@gmail.com>
2019-12-27 20:01:09 +0100

    First stable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -14,7 +15,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.DB_URL;
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
 app.use(cors())
