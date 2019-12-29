2d66fd30a9e8d57a81b1d66ed588dbc1ec211181
jompero <dani.jompero@gmail.com>
2019-12-29 17:13:53 +0100

    Connected Blog and User models, Json responses reflect this change

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,28 +1,48 @@
 const router = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 router.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 router.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
+  const blog = await (await Blog.findById(request.params.id)).populated('user', { username: 1, name: 1 })
   response.json(blog)
 })
   
 router.post('/', async (request, response, next) => {
-  if (!request.body.title || !request.body.url) { 
+  const { body } = request
+
+  if (!body.title || !body.url) { 
     const err = new Error()
     err.message = 'Blog must contain a title and an url'
     err.name = 'MissingArgumentsError'
     return next(err)
   }
-  const blog = new Blog(request.body)
-  if (!blog.likes) blog.likes = 0
 
-  const result = await blog.save()
-  response.status(201).json(result)
+  const user = await User.findOne({})
+  const likes = body.likes ? body.likes : 0
+  console.log("likes:", likes)
+
+  const blog = new Blog({
+    title: body.title,
+    url: body.url,
+    author: body.author,
+    user: user._id,
+    likes: likes
+  })
+
+  try {
+    const savedBlog = await blog.save()
+    // user.blogs = user.blogs ? user.blogs.concat(savedBlog._id) : [ savedBlog._id]
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:id', async (request, response, next) => {
--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
 router.get('/', async (request, response, next) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1 })
     response.json(users)
 })
 
